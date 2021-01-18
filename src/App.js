331f432219ffff835778bf16bff49cd60a81ddb2
Dean Leonard <deanleonard26@gmail.com>
2021-01-18 20:02:42 +0000

    Dean

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import StoriesShortcut from './components/StoriesShortcut/StoriesShortcut'
 import JoinTheMovement from './components/JoinTheMovement/Movement';
 import Footer from './components/Footer/Footer';
 import Instagram from './components/Instagram/Instagram';
+import Journey from './components/Journey/Journey';
 
 function App() {
   
   return (
     <div className="App">
+      <Journey/>
       <StoriesShortcut/>
       <JoinTheMovement/>
       <Instagram/>
---
+++ src/components/Journey/Journey.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Space+Mono&display=swap');
+</style>
+
+const Journey = () => {
+      return (
+            <JourneyWrapper>
+                <h1></h1>
+            </JourneyWrapper>
+      )
+}
+
+const JourneyWrapper = styled.div`
+background-color:lightgray;
+height:200px;
+margin-bottom:50px;
+
+`
+
+export default Journey;
