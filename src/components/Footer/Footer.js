a2be7270dfbfc70d66b57d11f3b0d7b229551f5c
Dean Leonard <deanleonard26@gmail.com>
2021-01-21 19:58:37 +0000

    Dean

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -36,7 +36,7 @@ const Footer = () => {
                 </Subscribe>
             </FooterWrapper>
             <Copyright>
-            <p>Copyright Authors© 2021</p>
+            <p>Copyright Authors© 202</p>
             </Copyright>
             </FooterDiv>
       )
