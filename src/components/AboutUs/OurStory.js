3c9a698aa1e5240646f819ba20b4000b50721ddc
Dean Leonard <deanleonard26@gmail.com>
2021-03-01 17:34:52 +0000

    Dean

--- src/components/AboutUs/OurStory.js
+++ src/components/AboutUs/OurStory.js
@@ -6,28 +6,31 @@ export default function OurStory() {
         <AboutUsSection>
         <MainHeading>
             <h3>Who We Are</h3>
-            <p>Contrary to popular belief, Lorem Ipsum is not simply 
+            <p>
+                Contrary to popular belief, Lorem Ipsum is not simply 
                 random text. It has roots in a piece of classical Latin 
                 literature from 45 BC, making it over 2000 years old. 
                 Richard McClintock, a Latin professor at Hampden-Sydney 
                 College in Virginia, looked up one of the more obscure Latin 
                 words.
             </p>
-            <p>Contrary to popular belief, Lorem Ipsum is not simply 
+            <p>
+                Contrary to popular belief, Lorem Ipsum is not simply 
                 random text. It has roots in a piece of classical Latin 
                 literature from 45 BC, making it over 2000 years old. 
                 Richard McClintock, a Latin professor at Hampden-Sydney 
                 College in Virginia, looked up one of the more obscure Latin 
                 words.
             </p>
-            <p>Contrary to popular belief, Lorem Ipsum is not simply 
+            <p>
+                Contrary to popular belief, Lorem Ipsum is not simply 
                 random text. It has roots in a piece of classical Latin 
                 literature from 45 BC, making it over 2000 years old. 
                 Richard McClintock, a Latin professor at Hampden-Sydney 
                 College in Virginia, looked up one of the more obscure Latin 
                 words.
             </p>
-            
+
         </MainHeading>
         <OurImpactSection>
             <OurImpactDescription>
